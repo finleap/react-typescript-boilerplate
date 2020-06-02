05a14ec853c2755f8fec44ccf06d74b0dc30638e
Andreas jantschnig <andreas.jantschnig@gmail.com>
2020-06-02 11:44:25 +0200

    boilerplate setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from "react";
+
+class App extends React.Component {
+  render = (): ReactNode => {
+    return <span>Welcome to React Typescript Boilerplate</span>;
+  };
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,185 @@
+const webpack = require("webpack");
+const path = require("path");
+const Dotenv = require("dotenv");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const { NODE_ENV, OPTIMIZED_BUILD } = process.env;
+const webpackDotEnvPath = `./config/.env.${NODE_ENV}`;
+const webpackEnvVars = Dotenv.config({ path: webpackDotEnvPath }).parsed;
+const localEnvironment = NODE_ENV === "local";
+const isOptimized = OPTIMIZED_BUILD === "true" || NODE_ENV === "production";
+const webpackWatch = localEnvironment && !isOptimized;
+const webpackMode = isOptimized ? "production" : "development";
+const webpackDevtool = isOptimized ? "hidden-source-map" : "cheap-eval-source-map";
+
+const config = {
+  mode: webpackMode,
+  watch: webpackWatch,
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[contenthash].js",
+  },
+  target: "web",
+  devtool: webpackDevtool,
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?x$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.ts?x$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              onlyCompileBundledFiles: true,
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images",
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ts?x|js?x)?$/,
+        exclude: /node_modules/,
+        enforce: "pre",
+        use: [
+          {
+            loader: "eslint-loader",
+            options: {
+              emitWarning: true,
+              emitError: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({ patterns: [{ from: "public" }] }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify({ NODE_ENV, ...webpackEnvVars }),
+      "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    nodeEnv: false,
+  },
+  devServer: {
+    compress: true,
+    contentBase: ["./src/public"],
+    historyApiFallback: true,
+    stats: {
+      assets: false,
+      cached: false,
+      cachedAssets: false,
+      children: false,
+      chunkModules: false,
+      chunkOrigins: false,
+      performance: false,
+      reason: false,
+      source: false,
+    },
+    watchContentBase: true,
+  },
+};
+
+if (localEnvironment) {
+  config.plugins.push(new ForkTsCheckerWebpackPlugin());
+}
+
+if (isOptimized) {
+  config.performance = {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+    hints: "warning",
+  };
+  config.optimization = {
+    ...config.optimization,
+    runtimeChunk: "single",
+    moduleIds: "hashed",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+    minimizer: [
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+        terserOptions: {
+          compress: {
+            drop_console: true,
+            pure_funcs: ["console.info", "console.debug", "console.warn"],
+          },
+          output: {
+            beautify: false,
+            comments: false,
+          },
+        },
+      }),
+    ],
+  };
+}
+
+module.exports = config;
